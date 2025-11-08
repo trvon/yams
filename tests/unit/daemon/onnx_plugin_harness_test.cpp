d491081bd4a96741016358efe4d9ff4265cd7147
trvon <git@trevon.dev>
2025-11-08 09:43:48 -0500

    v0.7.7 re-release

--- tests/unit/daemon/onnx_plugin_harness_test.cpp
+++ tests/unit/daemon/onnx_plugin_harness_test.cpp
@@ -7,7 +7,7 @@
 #include <cstdlib>
 #include <filesystem>
 #include <future>
-#include <unistd.h>  // For getpid()
+#include <unistd.h> // For getpid()
 #include <catch2/catch_test_macros.hpp>
 #include <yams/daemon/resource/plugin_host.h>
 #include <yams/plugins/model_provider_v1.h>
