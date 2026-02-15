0703a281883e3d0d16c7e27c4fe92b4f712e909a
trvon <git@trevon.dev>
2026-02-14 19:39:51 -0500

    fix: removing duplicate shim logic

--- tests/common/env_compat.h
+++ tests/common/env_compat.h
@@ -2,17 +2,4 @@
 
 #pragma once
 
-#include <cstdlib>
-
-#if defined(_WIN32)
-inline int setenv(const char* name, const char* value, int overwrite) {
-    if (!overwrite && std::getenv(name) != nullptr) {
-        return 0;
-    }
-    return _putenv_s(name, value ? value : "");
-}
-
-inline int unsetenv(const char* name) {
-    return _putenv_s(name, "");
-}
-#endif
+#include <yams/compat/unistd.h>
