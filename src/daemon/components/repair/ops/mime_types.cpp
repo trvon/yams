99db279e99dd4612c52623eb23c821f9718cd34d
trvon <me@trevon.dev>
2026-09-11 08:43:47 -0600

    refactor(repair): narrow operation dependencies
    Signed-off-by: trvon <me@trevon.dev>

--- src/daemon/components/repair/ops/mime_types.cpp
+++ src/daemon/components/repair/ops/mime_types.cpp
@@ -8,57 +8,17 @@
 #include "../repair_operation_support.h"
 #include "../repair_operations_internal.h"
 
-#include <spdlog/spdlog.h>
-#include <yams/core/repair_fsm.h>
-#include <yams/daemon/components/ConfigResolver.h>
-#include <yams/daemon/components/db_salvage.h>
-#include <yams/daemon/components/GraphComponent.h>
-#include <yams/daemon/components/InternalEventBus.h>
 #include <yams/daemon/components/MetadataWriteFacade.h>
-#include <yams/daemon/components/PostIngestQueue.h>
-#include <yams/daemon/components/ResourceGovernor.h>
-#include <yams/daemon/components/StateComponent.h>
-#include <yams/daemon/components/TuneAdvisor.h>
-#include <yams/daemon/components/TuningManager.h>
-#include <yams/daemon/components/TuningSnapshot.h>
-#include <yams/daemon/components/VectorIndexCoordinator.h>
-#include <yams/daemon/components/WriteCoordinator.h>
-#include <yams/daemon/metric_keys.h>
-#include <yams/daemon/resource/abi_symbol_extractor_adapter.h>
 #include <yams/detection/file_type_detector.h>
-#include <yams/extraction/content_extractor.h>
-#include <yams/extraction/extraction_util.h>
-#include <yams/integrity/repair_manager.h>
-#include <yams/metadata/document_metadata.h>
 #include <yams/metadata/metadata_repository.h>
-#include <yams/metadata/query_helpers.h>
-#include <yams/profiling.h>
-#include <yams/repair/embedding_repair_util.h>
-#include <yams/vector/sqlite_vec_backend.h>
-#include <yams/vector/vector_database.h>
 
-#include <sqlite3.h>
-#include <boost/asio/awaitable.hpp>
-#include <boost/asio/co_spawn.hpp>
-#include <boost/asio/detached.hpp>
-#include <boost/asio/steady_timer.hpp>
-#include <boost/asio/this_coro.hpp>
-#include <boost/asio/thread_pool.hpp>
-
-#include <algorithm>
 #include <atomic>
-#include <cctype>
-#include <cstring>
-#include <filesystem>
-#include <fstream>
+#include <cstdint>
 #include <memory>
-#include <mutex>
-#include <set>
-#include <span>
 #include <string>
-#include <thread>
-#include <unordered_map>
-#include <unordered_set>
+#include <string_view>
+#include <utility>
+#include <vector>
 
 namespace yams::daemon::repair {
 
